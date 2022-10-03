95e0f6af38c95fc82b7f67c26475785b5014f153
Andreas Themistocles <andreasthemistocles@hotmail.com>
2022-10-02 22:09:51 -0400

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,52 @@
+var express = require('express');
+var router = express.Router();
+
+// GET Home page
+router.get('/', function(req, res, next) {
+  res.render('index', 
+  { 
+    title: 'Home',
+  });
+});
+
+// GET Home page alternative
+router.get('/home', function(req, res, next) {
+  res.render('index', 
+  { 
+    title: 'Home',
+  });
+});
+
+// POST Home page
+router.post('/', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+// POST Home page alternative
+router.post('/home', function(req, res, next) {
+  res.render('index', { title: 'Home' });
+});
+
+// GET Contact page
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact' });
+});
+
+// GET About page
+router.get('/about', function(req, res, next) {
+  res.render('about', { title: 'About' });
+});
+
+// GET Services page
+router.get('/services', function(req, res, next) {
+  res.render('services', { title: 'Services' });
+});
+
+// GET Projects page
+router.get('/projects', function(req, res, next) {
+  res.render('projects', { title: 'Projects' });
+});
+
+
+
+module.exports = router;
