a4b8d43c056d6b470c5cfc0778f18673aef338e8
Andreas Themistocles <andreasthemistocles@hotmail.com>
2022-10-21 18:49:52 -0400

    new URI with MongoDB atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,6 @@
 // COMP229 Assignment 2 | config/dbs.js | Andreas Themistocles | 301251197 | October 20rd, 2022
 module.exports = 
 {
-    "URI": "mongodb://localhost/personal_website"
+    // "URI": "mongodb://localhost/personal_website"
+    "URI": "mongodb+srv://andreas:JWOJDpd8xh9uBZ5o@mongodbserver.jblequx.mongodb.net/personal_website?retryWrites=true&w=majority"
 }
